f29025fc3fec6375ce8a26024036403fe66ec018
Neeraj <Neeraj@DESKTOP-OID45S7>
2017-12-30 12:06:26 +0530

    added main method

--- src/main/java/com/boo/search/app/initializer/Application.java
+++ src/main/java/com/boo/search/app/initializer/Application.java
@@ -1,8 +1,14 @@
 package com.boo.search.app.initializer;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	public static void main(String[] args) throws Exception {
+    	System.out.println("####Application is called #######");
+        SpringApplication.run(Application.class, args);
+    }
 
 }
