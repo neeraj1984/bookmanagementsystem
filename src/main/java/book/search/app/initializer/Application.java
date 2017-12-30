c0898443b1ce7c3e991f65b82b299c641d7f1618
Neeraj <Neeraj@DESKTOP-OID45S7>
2017-12-30 12:09:13 +0530

    changed package name

--- src/main/java/book/search/app/initializer/Application.java
+++ src/main/java/book/search/app/initializer/Application.java
@@ -1,4 +1,4 @@
-package com.boo.search.app.initializer;
+package book.search.app.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
